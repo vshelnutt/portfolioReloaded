3b999aeb9343ca6a77253534f45e4153fb1fa344
vshelnutt <v.shelnutt@aol.com>
2018-07-16 21:03:29 -0500

    added pop up contact form

--- contact.js
+++ contact.js
@@ -2,10 +2,10 @@ $(function() {
   
   // contact form animations
   $('#contact').click(function() {
-    $('#contactForm').fadeToggle();
+    $('#hiddenForm').fadeToggle();
   })
   $(document).mouseup(function (e) {
-    var container = $("#contactForm");
+    var container = $("#hiddenForm");
 
     if (!container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
